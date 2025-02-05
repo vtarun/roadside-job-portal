9bea311c45bc03e8830a2c06230d92a14248b9aa
vtarun <vivektarun89@gmail.com>
2025-02-05 23:36:35 +0530

    Create profile template

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,14 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import { Button } from "./ui/button";
-import { BriefcaseBusiness, HeartIcon, PenBox } from "lucide-react";
+import { BriefcaseBusiness, HeartIcon, PenBox, Settings } from "lucide-react";
 
-import homeLogo from '/logo.png'
+import homeLogo from '/logo.png';
+import profile from '/profile.jpg';
 import { SignedIn, SignedOut, SignIn, UserButton, useUser } from '@clerk/clerk-react';
 
 const Header = () => {
   const [showSignIn, setShowSignIn] = useState(false);
   const {user} = useUser();
+  const [showProfile, setShowProfile] = useState(false);
 
   const [search, setSearch] = useSearchParams();
 
@@ -19,10 +21,16 @@ const Header = () => {
   }, [search]);
 
   const handleOverlayClick = (e) => {
+    console.log(e.target, e.currentTarget);
     if(e.target === e.currentTarget){
         setShowSignIn(false);
         setSearch({});
+        setShowProfile(false);
     }
+  }
+
+  const handleProfile = () => {
+    setShowProfile(prev => !prev);
   }
 
   return (
@@ -33,6 +41,9 @@ const Header = () => {
             </Link>
 
             <div className="flex gap-8">
+                <div className="w-10 h-10 rounded-full border border-red-200 overflow-hidden hover:shadow-white hover:border-red-600">
+                    <button onClick={handleProfile}><img src={profile} alt="profile"/></button>
+                </div>
                 <SignedOut>
                     <Button variant="outline" onClick={() => setShowSignIn(true)}>Login</Button>
                 </SignedOut>
@@ -68,6 +79,31 @@ const Header = () => {
         {showSignIn && <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50" onClick={handleOverlayClick}>
             <SignIn signUpForceRedirectUrl='/onboarding' fallbackRedirectUrl='/onboarding' />
             </div>  
+        }
+
+        {showProfile && 
+            <div className='fixed top-24 right-4' onClick={handleOverlayClick}>
+                <div className="rounded-sm border w-full h-auto border-red-200 sm:w-2/3 sm:h-2/3 md:w-1/2 md:h-1/2 lg:w-72 lg:h-80 lg:top-20 lg:right-0 bg-cyan-600">
+                    <div className='flex p-4 gap-4 items-center w-full'>
+                        <span className="w-14 h-14 rounded-full border border-red-200 overflow-hidden hover:shadow-white hover:border-red-600">
+                            <img src={profile} alt="profile"/>
+                        </span>
+                        <span>vit1@gmail.com</span>
+                    </div>
+                    <div className="border-t-[1px] border-[rgba(255,255,255,0.07)]flex p-4 gap-4 items-center w-full">
+                        <button>Manage account</button>
+                    </div>
+                    <div className="border-t-[1px] border-[rgba(255,255,255,0.07)] flex p-4 gap-4 items-center w-full">
+                        <button>My Jobs</button>
+                    </div>
+                    <div className="border-t-[1px] border-[rgba(255,255,255,0.07)] flex p-4 gap-4 items-center w-full">
+                        <button>Saved Jobs</button>
+                    </div>
+                    <div className="border-t-[1px] border-[rgba(255,255,255,0.07)] flex p-4 gap-4 items-center w-full">
+                        <button className="flex"><Settings size={20} />Sign Out</button>
+                    </div>
+                </div>                
+            </div>
         }
     </>
  );
